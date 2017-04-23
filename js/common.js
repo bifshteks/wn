3ead20eeb913630699d7f4a872b30567cfb8f2e6
bifshteks <artem.pif@mail.ru>
2017-04-23 05:52:12 +0300

    one more step

--- js/common.js
+++ js/common.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
 	$(".popup").magnificPopup();
 
-	$("#buy-item").submit(function() {
+	$(".send").submit(function() {
 		$.ajax ({
 			type: "POST",
-			url: "mail.php",
+			url: "js/mail.php",
 			data: $(this).serialize()
 		}).done(function() {
 			alert("Спасибо за заявку! Скоро мы вам перезвоним!")
